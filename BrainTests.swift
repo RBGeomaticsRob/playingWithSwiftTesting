aa4dd2af91ba504bf2d1293b775381c7db3bbdbc
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2016-02-04 14:13:55 +0000

    Added BrainTests

---
+++ BrainTests.swift
@@ -0,0 +1,24 @@
+//
+//  BrainTests.swift
+//  testapp
+//
+//  Created by Robert Bowers on 04/02/2016.
+//  Copyright © 2016 Robert Bowers. All rights reserved.
+//
+
+import XCTest
+
+class BrainTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+
+}
